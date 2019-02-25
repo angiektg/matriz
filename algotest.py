6300b796a082aa3f49970130708c9e0444761816
angiektg <antorres@unal.edu.co>
2019-02-25 14:30:34 -0500

    Version Graficos de Tiempo y Speedup

--- algotest.py
+++ algotest.py
@@ -10,7 +10,7 @@ mint = 10
 tiempos=[]
 
 plt.ion()
-fig = plt.figure()
+fig = plt.figure(figsize=(11,5))
 plt.axis([0,20,0,3])
 
 x = []
@@ -29,12 +29,20 @@ for hilos in range (1,nhilos+1):
     #x = np.linspace(1, hilos, hilos)
     x.append(2**(hilos-1))
     plt.clf()
-    plt.plot(x,tiempos,linestyle='-',marker='o', color='r')
-    for a,b in zip(x, tiempos): 
+    plt.suptitle('Multiplicacion de matriz 2048x2048', fontsize=14, fontweight='bold')
+    plt.subplot(121, title='Tiempo vs Numero de Hilos')
+    plt.plot(x,tiempos,linestyle='-',marker='+', markeredgecolor='b', color='r')
+    for a,b in zip(x, tiempos):
         plt.text(a, b, str(b)[0:5])
-    plt.suptitle('Tiempo vs Numero de hilos Matriz 2048x2048', fontsize=14, fontweight='bold')
     plt.xlabel("Numero de hilos")
     plt.ylabel("Tiempo")
+    plt.subplot(122, title='Speedup vs Numero de Hilos')
+    plt.plot(x,  (np.asarray(tiempos[0]))/np.asarray(tiempos)*x ,linestyle='-',marker='+', markeredgecolor='b', color='r')
+    for a,b in zip(x, (np.asarray(tiempos[0]))/np.asarray(tiempos)*x): 
+        plt.text(a, b, str(b)[0:5])
+    plt.xlabel("Numero de hilos")
+    plt.ylabel("Speedup")
+	
     plt.show()
     plt.pause(0.0001)
 
