304b64dac90cfb50b7bc5a9314796d0c37b79729
angiektg <antorres@unal.edu.co>
2019-02-25 01:31:25 -0500

    programa terminado, el numero de hilos a ejecutar se pasa como parametro al ejecutable. el numero de filas y columnas de la matriz es fijo.

--- mat.cpp
+++ mat.cpp
@@ -2,23 +2,26 @@
 #include <thread>
 #include <stdlib.h>
 
-const int ENE = 5;
+int NTHREADS;
+
+const unsigned long ENE = 256;                                  //numero de filas y columnas en la matriz
 
 
-class Matrix {
+
+class Matrix {      
     public:
 
-    int mt[ENE][ENE];
+    double mt[ENE][ENE];
 
-    Matrix(bool fill){
+    Matrix(bool fill){                              //constructor, se puede llenar la matriz con ceros o con numeros aleatorios
         for(int i=0; i<ENE; i++){
             for(int j=0;j<ENE;j++){
-		mt[i][j]=fill?rand()%10 + 1:0;
+		        mt[i][j]=fill?rand():0;
             }   
         }
     };
 
-    void show(){
+    void show(){                                    //mostrar la matriz
         std::cout << "\n";
         for(int i=0; i<ENE; i++){
             std::cout << "\n";
@@ -30,29 +33,34 @@ class Matrix {
     };
 };
 
-Matrix producto(Matrix A, Matrix B, int FR, int TO){
-Matrix c(false);
-int acc = 0;
-for(int i=FR;i<TO;i++){
-    for(int j=0;j<ENE;j++){
-            for(int k=0; k<ENE; k++){
-                acc = acc + (A.mt[i][k]*B.mt[k][j]);
-            }
-        c.mt[i][j] = acc;
-        acc = 0;
+void producto(Matrix A, Matrix B, Matrix &C, int rango){    //multiplicacion de matriz en filas determinadas por rango, ENE y NTHREADS, actualiza una matriz C
+double acc = 0;
+int inf = ENE/NTHREADS*rango;
+int sup = inf+ENE/NTHREADS;
+
+    for(int i=inf;i<sup;i++){
+        for(int j=0;j<ENE;j++){
+                for(int k=0; k<ENE; k++){
+                    acc = acc + (A.mt[i][k]*B.mt[k][j]);
+                }
+            C.mt[i][j] = acc;
+            acc = 0;
+        }
     }
-}
-return c;
 };
 
 
-int main (){
+int main (int argc, char *argv[]){
+    NTHREADS = atoi(argv[1]);
+    std::cout << "Numero de hilos: " << NTHREADS << "\n";
+    std::thread t[NTHREADS];
     Matrix a(true);
     Matrix b(true);
     Matrix r(false);
-    a.show(); 
-    b.show(); 
-    r = producto(a, b, 0, 2);
-    r.show();
+    //a.show(); 
+    //b.show(); 
+    for(int i=0;i<NTHREADS;i++) t[i] = std::thread (producto, a, b, std::ref(r), i);     
+    for(int i=0;i<NTHREADS;i++) t[i].join();
+    //r.show();
    return 0;
 };
